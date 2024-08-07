bb314fa046d598010c8d31a05d24f7a353d8642b
beomjun <liem@liem.zip>
2024-08-06 23:45:55 -0400

    Added google auth provider

--- umdm-webui/app/auth.ts
+++ umdm-webui/app/auth.ts
@@ -1,10 +1,12 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import Google from "@auth/core/providers/google";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
     // providers: [Google],
     providers: [
-        GitHub
+        GitHub,
+        Google
     ],
     callbacks: {
         async signIn({user, account, profile}): Promise<boolean | string> {
